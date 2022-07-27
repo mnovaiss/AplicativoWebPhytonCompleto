d9c6f767463f8064257433473427449a1530c2a5
mnovaiss <marcos.novais@gmail.com>
2022-07-27 21:51:43 +0000

    modificação  de 27-07-2022

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
 from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask("hello")
 
-@app.route("/")
-@app.route("/hello")
-def hello():
-    return "Hello Word"
+posts = [
+    {
+        "title": "O primeiro Post",
+        "body": "Aqui é o texto do post",
+        "author": "Marcos Novais",
+        "created": datetime(2022,7,25),
+    },
+    {
+        "title": "O segundo Post",
+        "body": "Aqui é o texto do post",
+        "author": "Jamile",
+        "created": datetime(2022,7,26),
+    },
+]
 
-@app.route("/meucontato")
-def meuContato():
-    return render_template("index.html")
+@app.route("/")
+def index():
+    return render_template("index.html", posts=posts)
+
+@app.route("/login")
+def login():
+    return render_template("index.html", posts=posts)
